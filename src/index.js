983afb236ef9a246933860b5b02ebe84a5f3a3f3
bux6942-beep <bux6942@gmail.com>
2026-04-28 16:26:31 +0800

    Create React entry file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// React 入口文件
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
